0a415081ffd251a85c37a449dafa99f38e56007e
hans-2002 <pattnaikhans@gmail.com>
2022-10-25 16:14:51 +0530

    Initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                Made with
+                <span style={
+                    { 'color': 'red', 'margin': '0 0.5rem', 'fontSize': '1.5rem' }
+                }>❤️</span>
+                by
+                <a href="https://hans-2002.github.io/" target="_blank">
+                    Hans
+                </a>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
